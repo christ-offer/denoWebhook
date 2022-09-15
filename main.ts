af31dc0ed087440210cc51abb9dfe6c19216eb45
Christoffer Holst <cholst@gmail.com>
2022-09-15 22:10:01 +0200

    cmon cmon cmon

--- main.ts
+++ main.ts
@@ -5,7 +5,6 @@ import {
   pullRequestReview,
 } from "https://deno.land/x/ghook@0.13.0/mod.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-import { SmallBot } from "https://deno.land/x/smallbot_matrix@0.1.2/mod.ts";
 
 
 // think this is posting the pulls to this url
@@ -13,20 +12,8 @@ const send = makeLarkSender("https://github-matrix.fly.dev/hook");
 
 const env = {
   githubSecret: Deno.env.get('GITHUB_KEY'),
-  matrixKey: Deno.env.get('MATRIX_KEY')
 }
-const client = new SmallBot({
-  accessToken: "syt_dGVzdGJvdG51bWVyb25l_QLPrwowZITZzzChegZtJ_1mweQC",
-  homeserverUrl: "https://matrix.org/",
-  eventHandler: async (client, roomId, event) => {
-      if (event.sender !== client.ownUserId) {
-          const profile = await client.getUserProfile(event.sender);
-          await client.sendRoomNotice(roomId, profile.displayname + ", you said: <b>" + event.content.body + "</b>");
-      }
-  }
-});
 
-await client.start();
 
 app("/webhook", { secret: env.githubSecret })
   // deno-lint-ignore no-explicit-any
