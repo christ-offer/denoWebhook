d55bc8fb0bcfd0bc9452e8e2e76a1ed451a334f3
Christoffer Holst <cholst@gmail.com>
2022-09-15 22:03:38 +0200

    cmon cmon cmon

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ import {
   pullRequestReview,
 } from "https://deno.land/x/ghook@0.13.0/mod.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+import { SmallBot } from "https://deno.land/x/smallbot_matrix@0.1.2/mod.ts";
 
 
 // think this is posting the pulls to this url
@@ -12,8 +13,20 @@ const send = makeLarkSender("https://github-matrix.fly.dev/hook");
 
 const env = {
   githubSecret: Deno.env.get('GITHUB_KEY'),
+  matrixKey: Deno.env.get('MATRIX_KEY')
 }
+const client = new SmallBot({
+  accessToken: "syt_dGVzdGJvdG51bWVyb25l_QLPrwowZITZzzChegZtJ_1mweQC",
+  homeserverUrl: "https://matrix.org/",
+  eventHandler: async (client, roomId, event) => {
+      if (event.sender !== client.ownUserId) {
+          const profile = await client.getUserProfile(event.sender);
+          await client.sendRoomNotice(roomId, profile.displayname + ", you said: <b>" + event.content.body + "</b>");
+      }
+  }
+});
 
+await client.start();
 
 app("/webhook", { secret: env.githubSecret })
   // deno-lint-ignore no-explicit-any
@@ -25,5 +38,6 @@ app("/webhook", { secret: env.githubSecret })
   .on("pull_request_review", (e : any) => send(pullRequestReview(e)))
   .on("push", (e : any) => {
     console.log("push", e)
+    client.sendRoomNotice("!phxFsseDFtRienTcza:matrix.org", "push")
   })
   
