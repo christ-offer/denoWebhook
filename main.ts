6001c17146489059060e0b6bd49f90bc2711beb3
Christoffer Holst <cholst@gmail.com>
2022-09-15 20:37:42 +0200

    second commit

--- main.ts
+++ main.ts
@@ -5,8 +5,9 @@ import {
   pullRequestReview,
 } from "https://deno.land/x/ghook@0.13.0/mod.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
 
-const send = makeLarkSender("localhost:3000");
+const send = makeLarkSender("");
 
 const env = {
   githubSecret: Deno.env.get('GITHUB_KEY'),
@@ -18,3 +19,7 @@ app("/webhook", { secret: env.githubSecret })
   // deno-lint-ignore no-explicit-any
   .on("pull_request_review", (e : any) => send(pullRequestReview(e)));
 
+
+
+  serve((req: Request) => new Response("Hello World"));
+  
