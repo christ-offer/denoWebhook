62a0ae357cea1600f6b551b91ab0fcc4b35ec110
Christoffer Holst <cholst@gmail.com>
2022-09-16 09:20:52 +0200

    ok

--- main.ts
+++ main.ts
@@ -6,23 +6,19 @@ import {
 } from "https://deno.land/x/ghook@0.13.0/mod.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 
-function makeLarkSender1(url: string) {
-  return (message?: string) => {
-    if (!message) return;
-
+function send(url: string, content: string) {
     return fetch(url, {
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
       },
-      body: "this is the body"
+      body: content
     });
-  };
 }
 
 
 // think this is posting the pulls to this url
-const send = makeLarkSender1("https://test-bot-bot.fly.dev/hook");
+// const send = makeLarkSender("https://test-bot-bot.fly.dev/hook");
 
 const env = {
   githubSecret: Deno.env.get('GITHUB_KEY'),
@@ -32,13 +28,13 @@ app("/webhook", { secret: env.githubSecret })
   // deno-lint-ignore no-explicit-any
   .on("pull_request", (e : any) => {
     console.log('a pull-request', e.action)
-    send(e.action)
+    send("https://test-bot-bot.fly.dev/hook", e.action)
   })
   // deno-lint-ignore no-explicit-any
   .on("pull_request_review", (e : any) => console.log(e.ref))
   .on("push", (e : any) => {
     console.log("push", e.ref)
-    send(e.ref)
+    send("https://test-bot-bot.fly.dev/hook", e.ref)
   })
 
   
