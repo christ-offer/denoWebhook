82f4e6478361122ce1a4dcc8dce61d766cdc0aa8
Christoffer Holst <cholst@gmail.com>
2022-09-16 08:32:11 +0200

    ok

--- main.ts
+++ main.ts
@@ -12,14 +12,7 @@ function makeLarkSender1(url: string) {
 
     return fetch(url, {
       method: "POST",
-      body: JSON.stringify({
-          elements: [
-            {
-              content: message,
-            },
-          ],
-        },
-      ),
+      body: message
     });
   };
 }
